b67b20a7b2d084120e265d1a8b3ab2a9e2c43747
Jeffreeyy <17844@ma-web.nl>
2015-11-04 11:00:29 +0100

    PoC - Enemies follow the waypoints
    Proof of Concept, enemies now follow the waypoints.

---
+++ Zzen 2D/Assets/Scripts/EnemyFollowTrack.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyFollowTrack : MonoBehaviour {
+
+	private Rigidbody2D _rb2d;
+	private int _targetedWaypoint = 0;
+	private Transform _waypoints;
+
+	private float _movementSpeed = 3f;
+
+	void Start () 
+	{
+		_rb2d = GetComponent<Rigidbody2D> ();
+		_waypoints = GameObject.Find("Waypoints").transform;
+	}
+	
+
+	void FixedUpdate () 
+	{
+		WaypointHandler ();
+		Debug.Log (_targetedWaypoint);
+	}
+
+	private void WaypointHandler()
+	{
+		Transform targetWaypoint = _waypoints.GetChild(_targetedWaypoint);
+		Vector3 relative = targetWaypoint.position - transform.position;
+		Vector3 movementNormal = Vector3.Normalize(relative);
+		float distanceToWaypoint = relative.magnitude;
+		float targetAngle = Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg - 90;
+
+		if(distanceToWaypoint < 0.1)
+		{
+			if(_targetedWaypoint + 1 < _waypoints.childCount)
+			{
+				_targetedWaypoint++;
+			}
+		}
+		else
+		{
+			_rb2d.AddForce(new Vector2(movementNormal.x, movementNormal.y) * _movementSpeed);
+		}
+		transform.rotation = Quaternion.Euler(0, 0, targetAngle);
+	}
+}
