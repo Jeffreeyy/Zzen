187a5afecc898c60dd29dd7232e8acce64a91a82
tommtom9 <tommtom9@live.nl>
2015-11-23 11:36:20 +0100

    Main Menu and About Menu

---
+++ Zzen 2D/Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonFunctions : MonoBehaviour {
+
+    public void LoadGame()
+    {
+            Application.LoadLevel("Game");
+    }
+
+    public void ToAboutMenu()
+    {
+            Application.LoadLevel("Aboutscene");
+    }
+
+    public void ToMainMenu()
+
+    {
+        Application.LoadLevel("MainMenu");
+    }
+}
