e50d6fd451782dfd086127cc21b4b3a311475c2a
tommtom9 <tommtom9@live.nl>
2015-11-24 08:50:57 +0100

    Updated map

--- Zzen 2D/Assets/Scripts/EnemyMovement.cs
+++ Zzen 2D/Assets/Scripts/EnemyMovement.cs
@@ -6,8 +6,8 @@ public class EnemyMovement : MonoBehaviour {
 	private Rigidbody2D _rb2d;
 	private int _targetedWaypoint = 0;
 	private Transform _waypoints;
-
-	[SerializeField] private float _movementSpeed = 4f;
+    private float _offset = 2f;
+    [SerializeField] private float _movementSpeed = 4f;
 
 	void Start () 
 	{
@@ -24,7 +24,7 @@ public class EnemyMovement : MonoBehaviour {
 	private void WaypointHandler()
 	{
 		Transform targetWaypoint = _waypoints.GetChild(_targetedWaypoint);
-		Vector3 relative = targetWaypoint.position - transform.position;
+		Vector3 relative = (targetWaypoint.position + new Vector3(0,_offset,0)) - transform.position;
 		Vector3 movementNormal = Vector3.Normalize(relative);
 		float distanceToWaypoint = relative.magnitude;
 		float targetAngle = Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg - 90;
@@ -40,7 +40,7 @@ public class EnemyMovement : MonoBehaviour {
 		{
 			_rb2d.AddForce(new Vector2(movementNormal.x, movementNormal.y) * _movementSpeed);
 		}
-		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, targetAngle), Time.deltaTime * 3f);
+		//transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, targetAngle), Time.deltaTime * 3f);
 	}
 
 	void OnCollisionEnter2D(Collision2D col)
