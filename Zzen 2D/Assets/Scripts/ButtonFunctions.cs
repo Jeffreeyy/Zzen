b1f3988ea0056908786f3d017a80b4f46adac698
tommtom9 <tommtom9@live.nl>
2015-11-26 08:49:10 +0100

    Added alot of animations and art into the game

--- Zzen 2D/Assets/Scripts/ButtonFunctions.cs
+++ Zzen 2D/Assets/Scripts/ButtonFunctions.cs
@@ -4,9 +4,19 @@ using UnityEngine.UI;
 
 public class ButtonFunctions : MonoBehaviour {
 
+    private GameObject pauseButton;
+
+    void Start()
+    {
+        if (Application.loadedLevel == 3)
+        {
+            pauseButton = GameObject.Find("PauseMenu");
+            pauseButton.SetActive(false);
+        }
+    }
     public void LoadGame()
     {
-            Application.LoadLevel("Game");
+            Application.LoadLevel("GameWithArt");
     }
 
     public void ToAboutMenu()
@@ -15,8 +25,19 @@ public class ButtonFunctions : MonoBehaviour {
     }
 
     public void ToMainMenu()
-
     {
         Application.LoadLevel("MainMenu");
+    }
+
+    public void PauseMenu()
+    {
+        Time.timeScale = 0;
+        pauseButton.SetActive(true);
+    }
+
+    public void HideMenu()
+    {
+        Time.timeScale = 1;
+        pauseButton.SetActive(false);
     }
 }
