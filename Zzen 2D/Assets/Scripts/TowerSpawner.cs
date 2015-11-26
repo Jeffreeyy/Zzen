0c8660cd7026749435e326b92ea58d034ad5208d
Jeffreeyy <17844@ma-web.nl>
2015-11-27 00:18:51 +0100

    Added font, announcer, some other ui components.

--- Zzen 2D/Assets/Scripts/TowerSpawner.cs
+++ Zzen 2D/Assets/Scripts/TowerSpawner.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TowerSpawner : MonoBehaviour {
 
-    [SerializeField]private GameObject PlayerStats;
+    [SerializeField] private GameObject PlayerStats;
 	[SerializeField] private GameObject _towerPrefab;
+    [SerializeField] private Text _updateText;
 	private GameObject _tower;
+    private bool _isUpdatingText = false;
 
 	private bool canPlaceTower()
 	{
@@ -22,7 +25,23 @@ public class TowerSpawner : MonoBehaviour {
 			    Instantiate(_towerPrefab, transform.position, Quaternion.identity);
                 PlayerStats.GetComponent<PlayerController>().seeds -= 50;
             }
+            else
+            {
+                if (_isUpdatingText == false)
+                {
+                    StartCoroutine(EditUpdateText());
+                }
+            }
 
 		}
 	}
+
+    IEnumerator EditUpdateText()
+    {
+        _isUpdatingText = true;
+        _updateText.text = "Not enough seeds! You need 50 seeds to plant!";
+        yield return new WaitForSeconds(3);
+        _updateText.text = " ";
+        _isUpdatingText = false;
+    }
 }
