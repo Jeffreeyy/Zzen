5aaedde1c591cd06722de3ede63b04d13e62c1c0
tommtom9 <tommtom9@live.nl>
2015-11-27 08:42:11 +0100

    Added basic music

---
+++ Zzen 2D/Assets/Scripts/GameOverScore.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScore : MonoBehaviour {
+
+    private float score;
+
+    void Start()
+    {
+
+    }
+}
---
+++ Zzen 2D/Assets/Scripts/Soundhandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Soundhandler : MonoBehaviour {
+
+    private GameObject _audioHandler;
+    private AudioSource _main_Tune;
+    private AudioSource _main_Menu_Tune;
+    private AudioSource _Ambiance_Sound;
+
+	// Use this for initialization
+	void Start () {
+
+        if (Application.loadedLevelName == "GameWithArt")
+        {
+            _main_Tune = GameObject.Find("Main_Tune").GetComponent<AudioSource>();
+            PlayMain_Tune();
+        }
+
+        if (Application.loadedLevelName == "MainMenu" || Application.loadedLevelName == "AboutScene")
+        {
+            _main_Menu_Tune = GameObject.Find("Main_Menu_Amb").GetComponent<AudioSource>(); ;
+            _Ambiance_Sound = GameObject.Find("Main_Menu_Tune").GetComponent<AudioSource>(); ;
+            PlayMain_Menu_Tunes();
+        }
+    }
+
+	void Update () {
+	
+	}
+
+    public void PlayMain_Tune()
+    {
+        _main_Tune.Play();
+    }
+
+    public void PlayMain_Menu_Tunes()
+    {
+        _Ambiance_Sound.Play();
+        _main_Menu_Tune.Play();
+    }
+}
